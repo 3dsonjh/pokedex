c5ef0f57433697c5127cfb00fa6ed145f975f3c2
Edson Alves <alves171@gmail.com>
2023-04-18 21:45:12 -0300

    Fim da aula de 18/04/2023

--- src/Tela.js
+++ src/Tela.js
@@ -1,21 +1,74 @@
+import axios from "axios"
+import { useState } from "react";
+
+
+
 export default function Tela()
 {
+    const [pokemon, setPokemon] = useState(null);
+    const [pesquisa, setPesquisa] = useState("");
+    const [alertErro, setAlertErro] = useState(false);
+
+    //let alertErro = "" 
+
+    async function carregaPokemon()
+    {
+        try{
+            setPokemon(null);
+            /*const pokemon = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");*/
+            /*const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon/"+pesquisa);*/
+            const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pesquisa}`);
+            /*console.log(data);*/
+            setPokemon(data);
+        }
+        catch(erro) {
+            /*console.log(erro);*/
+            setAlertErro(true);
+        }
+    }
+
+    function pokeNome(evento){
+        setPesquisa(evento.target.value);
+    }
+
+
+     /* exibe o pokemon depois de ser feita uma pesquisa */
+    let cardPokemon = "";
+    if(pokemon!=null){
+        cardPokemon = <div className="card">
+                <div className="card-header">
+                    Nome: <strong>{pokemon.name}</strong>
+                </div>
+                <img src={pokemon.sprites.other['official-artwork'].front_default} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <p>Altura: <strong>{pokemon.height}</strong></p>
+                    <p>Peso: <strong>{pokemon.weight}</strong></p>
+                    <div>
+                        Habilidades:
+                        <ul>
+                            {
+                                pokemon.abilities.map(function(item)
+                                {
+                                    return (<li key={item.ability.name}>{item.ability.name}</li>)
+                                })
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </div>;
+    }
+
     return (
         <div>
-            <div class="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Digite o nome" />
-                <button className="btn btn-outline-secondary" type="button">Pesquisar</button>
-            </div>
-            <div className="card">
-                <div className="card-header">
-                    Nome do Pokemon
-                </div>
-                <img src="https://placehold.co/400x200" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    
-                    detalhes
-                </div>
-            </div>
+            <div className="input-group mb-3">
+                <input type="text" onChange={pokeNome} className="form-control" placeholder="Digite o nome" />
+                <button disabled={pesquisa == ""} onClick={carregaPokemon} className="btn btn-outline-secondary" type="button">Pesquisar</button>
+            </div> 
+            {cardPokemon} 
+            {(alertErro==true)? <div className="alert alert-danger" role="alert">
+            Bichinho não encontrado!
+            </div>:""}   
         </div>
+        
     )
 }
